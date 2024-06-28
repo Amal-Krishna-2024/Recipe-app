8fa9d79a3b6e6a9332f71351a5f9490db9bf34fd
12345amal12345 <amalgkrishnan204@gmail.com>
2024-06-28 22:02:22 +0530

    changed

--- src/components/AddRecipe.jsx
+++ src/components/AddRecipe.jsx
@@ -6,14 +6,17 @@ const AddRecipe = () => {
       <div className="container">
         <div className="row">
             <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                <h1> HOTEL MOONLIGHT </h1>
+            <img src="https://phsengineersltd.co.uk/wp-content/uploads/2019/07/Hotel-Kitchen-Maintenance-Tips-1024x538.jpg" alt="" />
 
 <div className="row g-3">
     <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-        <label htmlFor="" className="form-label">Title</label>
+    
+        <label htmlFor="" className="form-label">Recipe Title</label>
         <input type="text" className="form-control" />
     </div>
     <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-    <label htmlFor="" className="form-label">Creator</label>
+    <label htmlFor="" className="form-label">Recipe Creator</label>
     <input type="text" className="form-control" />
     </div>
     <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
